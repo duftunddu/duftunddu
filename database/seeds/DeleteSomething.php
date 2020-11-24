ead8dfc9be7b5e87cd9319636002c9d3b417a755
Haise17 <sam_here@outlook.com>
2020-11-24 23:13:42 +0500

    44b | Fixed stuff.

---
+++ database/seeds/DeleteSomething.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Fragrance_Brand;
+use Illuminate\Database\Seeder;
+
+class DeleteSomething extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Fragrance_Brand::where('name',"Garçon")->first()->delete();
+    }
+}
