87d6c2adf56b114e9b8bbea16a0780223155392f
Abdul Samad <sam_here@outlook.com>
2020-12-05 23:52:48 +0500

    44v | Fixed app css and js files.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/layout.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
