cb9b47b42ad5641ea0c5be5f503ac2e189db2cf3
Abdul Samad <sam_here@outlook.com>
2021-02-05 14:06:04 +0500

    44aga | Preloader values adjusted.

--- public/js/preloader.js
+++ public/js/preloader.js
@@ -8,10 +8,10 @@ $(document).ready(function () {
 
         if ($('#ctn-preloader').hasClass('loaded')) {
             // Es para que una vez que se haya ido el preloader se elimine toda la seccion preloader
-            $('#preloader').delay(0).queue(function () {
+            $('#preloader').delay(1000).queue(function () {
                 $(this).remove();
             });
         }
-    }, 0);
+    }, 3000);
 
 });
