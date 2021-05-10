12b31a2c218c039cbed78321228aad5f9735f7f3
Abdul Samad <sam_here@outlook.com>
2021-05-10 18:52:14 +0500

    44agwp | Retrained model.

--- app/Helper/Fragrance_Review_Helper.php
+++ app/Helper/Fragrance_Review_Helper.php
@@ -42,13 +42,13 @@ class Fragrance_Review_Helper {
         // $weather_data               =   $helper->get_weather_average_data();
 
         // For debugging
-        // $fragrance_review_helper->save_longevity_template($fragrance_data, $profile_data, $weather_data);
+        $fragrance_review_helper->save_longevity_template($fragrance_data, $profile_data, $weather_data);
 
         // Calculating
         if (App::environment('local')) {
             // The environment is local
 
-            $process = new Process([ 'C:\Anaconda3\Scripts\activate_duft_und_du.bat', 'longevity_prediction.py',
+            $process = new Process([ 'C:\Anaconda3\Scripts\activate_duft_und_du.bat', 'longevity_prediction_random_forest.py',
                 $fragrance_data, $profile_data, $weather_data], null, [ 'PYTHONHASHSEED' => "1", 'SystemRoot' => "C:\\WINDOWS", 'Home' => 'C:\\Anaconda3\\envs\\duft_und_du']);
         }
         else {
