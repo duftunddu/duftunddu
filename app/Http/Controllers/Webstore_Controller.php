8a9aa11f1a5012d4cd7700ab4f8919016dde37e2
Abdul Samad <sam_here@outlook.com>
2021-05-10 20:51:00 +0500

    44agwo | Testing.

--- app/Http/Controllers/Webstore_Controller.php
+++ app/Http/Controllers/Webstore_Controller.php
@@ -22,7 +22,7 @@ use App\Climate;
 use App\Season;
 
 use App\User_Fragrance_Review;
-use App\Store_Customer_Feature_Logs;
+use App\Store_Customer_Feature_Log;
 
 use Validator;
 use Illuminate\Validation\Rule;
