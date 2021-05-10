123637ffe24f518593a1b6353d126fed23fd68a3
Abdul Samad <sam_here@outlook.com>
2021-05-10 20:45:07 +0500

    44agwo | Testing.

--- routes/web.php
+++ routes/web.php
@@ -97,9 +97,9 @@ Route::post('/webstore_register', "Webstore_Request_Controller@store");
 Route::get('/webstore_call/{api_key}/{user_ip_address}/{brand}/{fragrance}/{fragrance_type}/{theme}', "Webstore_Controller@webstore_call");
 
 
+Route::get('/webstore_profile/some', "Webstore_Controller@store_profile");
 Route::get('/webstore_profile/{store_id?}', "Webstore_Controller@add_profile");
 // Route::post('/webstore_profile', "Webstore_Controller@store_profile");
-Route::get('/webstore_profile/some', "Webstore_Controller@store_profile");
 // Route::post('/webstore_profile', "Controller@meow");
 
 Route::get('/webstore_fragrance/{brand}/{fragrance}', "Webstore_Controller@show_fragrance");
