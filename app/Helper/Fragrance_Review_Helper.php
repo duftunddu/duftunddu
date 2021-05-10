fa0bec972119584046fb80887c6b0da81e2fcfa3
Abdul Samad <sam_here@outlook.com>
2021-05-10 18:57:08 +0500

    44agwp | Retrained model.

--- app/Helper/Fragrance_Review_Helper.php
+++ app/Helper/Fragrance_Review_Helper.php
@@ -41,6 +41,8 @@ class Fragrance_Review_Helper {
         $weather_data               =   json_encode($helper->get_weather_average_data());
         // $weather_data               =   $helper->get_weather_average_data();
 
+        dd($fragrance_data, $profile_data, $weather_data);
+
         // For debugging
         // $fragrance_review_helper->save_longevity_template($fragrance_data, $profile_data, $weather_data);
 
